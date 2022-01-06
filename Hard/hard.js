a3388da476d0157e36ec33aaab9b65d63bd0b5a5
Isaac Santos <Isaac@R2Hs-MacBook-Pro.local>
2022-01-06 17:53:15 -0500

    Finished challenges

--- Hard/hard.js
+++ Hard/hard.js
@@ -3,48 +3,35 @@
 // Addition
 function add(){
     var num1 = 9
-    var num2 = 4
+    var num2 = 3
     var answer = num1+num2;
     console.log(num1 + " + " + num2 + " = " + answer)
 }
 add();
 
 //Subtraction
-var subtract = function(num1, num2){
-    if (choice === "Subtract"){
-        var num1 = Number(prompt("Choose first number."))
-        var num2 = Number(prompt("Choose second number."))
-        var answer = num1-num2;
-        alert(answer)
-        console.log(num1 + " - " + num2 + " = " + answer)
-    }
+function subtract(){
+    var num1 = 9
+    var num2 = 3
+    var answer = num1-num2;
+    console.log(num1 + " - " + num2 + " = " + answer)
 }
+subtract();
 
 //Multiplication
-var multiply = function(num1, num2){
-    if (choice === "Multiply"){
-        var num1 = Number(prompt("Choose first number."))
-        var num2 = Number(prompt("Choose second number."))
-        var answer = num1*num2;
-        alert(answer)
-        console.log(num1 + " * " + num2 + " = " + answer)
-    }
+function multiply(){
+    var num1 = 9
+    var num2 = 3
+    var answer = num1*num2;
+    console.log(num1 + " * " + num2 + " = " + answer)
 }
+multiply();
 
 //Division
-var divide = function(num1, num2){
-    if (choice === "Divide"){
-        var num1 = Number(prompt("Choose first number."))
-        var num2 = Number(prompt("Choose second number."))
-        var answer = num1/num2;
-        alert(answer)
-        console.log(num1 + " / " + num2 + " = " + answer)
-    }
+function divide(){
+    var num1 = 9
+    var num2 = 3
+    var answer = num1/num2;
+    console.log(num1 + " / " + num2 + " = " + answer)
 }
-
-// // Create functions
-// var add = function(num1, num2){
-//     if (choice == "Add"){
-//         alert(num1+num2)
-//     }
-// }
+divide();
--- vHard/vHard.js
+++ vHard/vHard.js
@@ -1,46 +1,60 @@
-// Create variables
+//VERY HARD: Create a simple calculator that prompts the user for a number, an operator (either +, -, * or /) and another number, and then uses the functions created in the hard challenge to output the value in sentence form. Example output: "3 + 4 = 7"
+
+
+// Calculator Prompt
 var choice = prompt('Type to choose: "Add" "Subtract" "Multiply" "Divide"');
 
+// Direct to +,-,*,/
+if (choice === "Add"){
+    add()
+} else if (choice === "Subtract"){
+    subtract()
+} else if (choice === "Multiply"){
+    multiply()
+} else (choice === "Divide");{
+    divide()
+}
+
 // Addition
-//var add = function(choice){
+function add(){
     if (choice === "Add"){
-        var num1 = Number(prompt("Choose first number."))
-        var num2 = Number(prompt("Choose second number."))
-        var answer = num1+num2;
-        alert(answer)
-        console.log(num1 + " + " + num2 + " = " + answer)
+    var num1 = Number(prompt("Choose first number."))
+    var num2 = Number(prompt("Choose second number."))
+    var answer = num1+num2;
+    alert(answer)
+    console.log(num1 + " + " + num2 + " = " + answer)
     }
-///}
+}
 
 //Subtraction
-//var subtract = function(num1, num2){
+function subtract(){
     if (choice === "Subtract"){
-        var num1 = Number(prompt("Choose first number."))
-        var num2 = Number(prompt("Choose second number."))
-        var answer = num1-num2;
-        alert(answer)
-        console.log(num1 + " - " + num2 + " = " + answer)
+    var num1 = Number(prompt("Choose first number."))
+    var num2 = Number(prompt("Choose second number."))
+    var answer = num1-num2;
+    alert(answer)
+    console.log(num1 + " - " + num2 + " = " + answer)
     }
-//}
+}
 
 //Multiplication
-//var multiply = function(num1, num2){
+function multiply(){
     if (choice === "Multiply"){
-        var num1 = Number(prompt("Choose first number."))
-        var num2 = Number(prompt("Choose second number."))
-        var answer = num1*num2;
-        alert(answer)
-        console.log(num1 + " * " + num2 + " = " + answer)
+    var num1 = Number(prompt("Choose first number."))
+    var num2 = Number(prompt("Choose second number."))
+    var answer = num1*num2;
+    alert(answer)
+    console.log(num1 + " * " + num2 + " = " + answer)
     }
-//}
+}
 
 //Division
-//var divide = function(num1, num2){
+function divide(){
     if (choice === "Divide"){
-        var num1 = Number(prompt("Choose first number."))
-        var num2 = Number(prompt("Choose second number."))
-        var answer = num1/num2;
-        alert(answer)
-        console.log(num1 + " / " + num2 + " = " + answer)
+    var num1 = Number(prompt("Choose first number."))
+    var num2 = Number(prompt("Choose second number."))
+    var answer = num1/num2;
+    alert(answer)
+    console.log(num1 + " / " + num2 + " = " + answer)
     }
-//}
+}
